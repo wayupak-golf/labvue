4cf3ae95ad208e30bf465ad55ae3c7af08544f18
wayupak <wayupak_w@cmu.ac.th>
2024-03-11 00:40:58 +0700

    event
    lab-7

--- main.js
+++ main.js
@@ -13,16 +13,30 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green'},
-            {id: 2235, color: 'blue'}
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
         ])
+        const cart = ref(0)
+        function addToCart() {
+            cart.value += 1
+        }
+
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+
+
+
         return { 
             product, 
             image,
             inStock,
             inventory,
             details,
-            variants
+            variants,
+            cart,
+            addToCart,
+            updateImage
         }
     }  
 }).mount('#app')
