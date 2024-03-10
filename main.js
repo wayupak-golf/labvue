df602d34faa8858c6b402b7bf99026d0dcddca5e
wayupak <wayupak_w@cmu.ac.th>
2024-03-10 23:38:22 +0700

    v-for
    lab-6

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const inStock = ref(false)
-        const inventory = ref(0)
+        const inventory = ref(100)
+        const details = ref([
+            '50% cotton', 
+            '30% wool', 
+            '20% polyester'
+        ])
+        const variants = ref([
+            {id: 2234, color: 'green'},
+            {id: 2235, color: 'blue'}
+        ])
         return { 
             product, 
             image,
             inStock,
-            inventory
+            inventory,
+            details,
+            variants
         }
     }  
 }).mount('#app')
