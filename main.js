5ffaa39375785a2905d67f691c2ab293a3c50525
wayupak <wayupak_w@cmu.ac.th>
2024-03-11 20:02:57 +0700

    update add to cart
    lab-9

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const product = 'Socks'
-const {createApp, ref} = Vue
+const {createApp, ref, computed} = Vue
 
 createApp({
     setup() {
         const product = ref('Boots')
         const brand = ref('SE 331')
-        const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(false)
+        // const image = ref('./assets/images/socks_green.jpg')
+        // const inStock = ref(false)
         const inventory = ref(100)
         const details = ref([
             '50% cotton', 
@@ -14,25 +14,53 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
         ])
+
+        const selectedVariant = ref(0)
         const cart = ref(0)
+
+        const onSale = ref(true)
+
         function addToCart() {
             cart.value += 1
         }
 
+        const title = computed(() => {
+            return brand.value + ' ' + brand.value
+        })
+
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
 
 
         function updateImage(variantImage) {
             image.value = variantImage
         }
 
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+
+        const saleStatus = computed(() => {
+            if (onSale.value) {
+                return `${brand.value} ${product.value} is on sale!`;
+            } else {
+                return '';
+            }
+        });
+        
+
 
 
         return { 
-            product, 
-            brand,
+            title,
             image,
             inStock,
             inventory,
@@ -40,7 +68,9 @@ createApp({
             variants,
             cart,
             addToCart,
-            updateImage
+            updateImage,
+            updateVariant,
+            saleStatus
         }
     }  
 }).mount('#app')
