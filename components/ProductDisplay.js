497efe0f36328fa59bc506ab350e5f971cef6141
wayupak <wayupak_w@cmu.ac.th>
2024-03-11 20:51:58 +0700

    component
    lab-10

---
+++ components/ProductDisplay.js
@@ -0,0 +1,108 @@
+const productDisplay = { 
+    template: `<div class = "product-display">
+    <div class = "produc-container">
+        <div class = "product-image">
+            <!--image goes here-->
+            <img :src="image">
+        </div>
+    </div>
+</div>
+<div class = "product-info">
+    <h1>{{title}}</h1>
+    <p v-if="inventory > 10 ">In Stock</p>
+    <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out</p>
+    <p>Shipping:{{shipping}}</p>
+    <ul>
+        <li v-for="detail in details">{{detail}}</li>
+    </ul>
+    <div v-for="(variant, index) in variants" :key="variant.id"
+    @mouseover="updateVariant(index)"
+    class="color-circle" :style="{ backgroundColor: variant.color }">
+    </div>
+    <button class="button" :disabled="!inStock" @click="addToCart"
+    :class="{ disabledButton: !inStock }">Add To Cart</button>
+</div>`,
+props: {
+    premium: Boolean
+},
+
+setup(props) {
+    const shipping = computed(() => {
+        if (props.premium) {
+            return 'Free'
+        } else {
+            return 30
+        }
+    })
+    const product = ref('Boots')
+    const brand = ref('SE 331')
+    // const image = ref('./assets/images/socks_green.jpg')
+    // const inStock = ref(false)
+    const inventory = ref(100)
+    const details = ref([
+        '50% cotton', 
+        '30% wool', 
+        '20% polyester'
+    ])
+    const variants = ref([
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
+    ])
+
+    const selectedVariant = ref(0)
+    const cart = ref(0)
+
+    const onSale = ref(true)
+
+    function addToCart() {
+        cart.value += 1
+    }
+
+    const title = computed(() => {
+        return brand.value + ' ' + brand.value
+    })
+
+    const image = computed(() => {
+        return variants.value[selectedVariant.value].image
+    })
+
+    const inStock = computed(() => {
+        return variants.value[selectedVariant.value].quantity
+    })
+
+
+    function updateImage(variantImage) {
+        image.value = variantImage
+    }
+
+    function updateVariant(index) {
+        selectedVariant.value = index;
+    }
+
+    const saleStatus = computed(() => {
+        if (onSale.value) {
+            return `${brand.value} ${product.value} is on sale!`;
+        } else {
+            return '';
+        }
+    });
+    
+
+
+
+    return { 
+        title,
+        image,
+        inStock,
+        inventory,
+        details,
+        variants,
+        cart,
+        addToCart,
+        updateImage,
+        updateVariant,
+        shipping,
+        saleStatus
+    }
+}  
+}
--- main.js
+++ main.js
@@ -1,76 +1,17 @@
 const product = 'Socks'
 const {createApp, ref, computed} = Vue
 
-createApp({
+const app = createApp ({
     setup() {
-        const product = ref('Boots')
-        const brand = ref('SE 331')
-        // const image = ref('./assets/images/socks_green.jpg')
-        // const inStock = ref(false)
-        const inventory = ref(100)
-        const details = ref([
-            '50% cotton', 
-            '30% wool', 
-            '20% polyester'
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
-        ])
-
-        const selectedVariant = ref(0)
         const cart = ref(0)
-
-        const onSale = ref(true)
-
-        function addToCart() {
-            cart.value += 1
-        }
-
-        const title = computed(() => {
-            return brand.value + ' ' + brand.value
-        })
-
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })
-
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity
-        })
-
-
-        function updateImage(variantImage) {
-            image.value = variantImage
-        }
-
-        function updateVariant(index) {
-            selectedVariant.value = index;
-        }
-
-        const saleStatus = computed(() => {
-            if (onSale.value) {
-                return `${brand.value} ${product.value} is on sale!`;
-            } else {
-                return '';
-            }
-        });
-        
-
-
-
+        const premium = ref(true)
         return { 
-            title,
-            image,
-            inStock,
-            inventory,
-            details,
-            variants,
             cart,
-            addToCart,
-            updateImage,
-            updateVariant,
-            saleStatus
+            premium 
         }
     }  
-}).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+
+app.mount('#app')
